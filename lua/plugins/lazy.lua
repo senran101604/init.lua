ac77c6a522925408f2df1e03b81ac90c189180f1
senran101604 <senran101604@gmail.com>
2023-12-07 10:37:55 +0800

    Added: nvim-surround

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -124,5 +124,15 @@ require("lazy").setup({
       -- 'folke/neodev.nvim',
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 })
 
