5b31bd0dbbed3ee66814affaa59c8faeb7c2231f
SenRan <senran101604@gmail.com>
2023-12-15 00:18:19 +0800

    Clean

--- init.lua
+++ init.lua
@@ -9,16 +9,3 @@
 
 require('core')
 require('plugins')
-
--- Colorscheme
--- TeraFox from nightfox.nvim
-vim.cmd.colorscheme 'terafox'
-vim.cmd("hi String gui=None")
-vim.cmd("hi Normal guibg=#151515")
-vim.cmd("hi CursorLine guibg=#1d2021")
-vim.cmd("hi! link NormalNC Normal")
--- Melange
--- vim.cmd.colorscheme 'melange'
--- vim.cmd("hi String gui=None")
--- vim.cmd("hi Normal guibg=#151515")
--- vim.cmd("hi ColorColumn guibg=#292522")
