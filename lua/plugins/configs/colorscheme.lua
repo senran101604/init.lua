63fd5fccf67c7eeb641ba29fc81001333a9cbb35
SenRan <senran101604@gmail.com>
2023-12-20 22:21:09 +0800

    Update colorscheme

--- lua/plugins/configs/colorscheme.lua
+++ lua/plugins/configs/colorscheme.lua
@@ -4,10 +4,11 @@ local current_colorscheme = vim.g.colors_name
 
 -- Melange
 if current_colorscheme == "melange" then
-    -- vim.cmd("hi Normal guibg=#151515")
+    vim.cmd("hi Normal guibg=#151515")
+    vim.cmd("hi NormalFloat guibg=#292522")
     -- vim.cmd("hi ColorColumn guibg=#292522")
     vim.cmd("hi String gui=None")
-    vim.cmd("hi Normal guibg=None")
+    -- vim.cmd("hi Normal guibg=None")
     vim.cmd("hi ColorColumn guibg=None")
 -- TeraFox from nightfox.nvim
 elseif current_colorscheme == "terafox" then
@@ -15,4 +16,9 @@ elseif current_colorscheme == "terafox" then
     vim.cmd("hi Normal guibg=#151515")
     vim.cmd("hi CursorLine guibg=#1d2021")
     vim.cmd("hi! link NormalNC Normal")
+elseif current_colorscheme == "gruvbox" then
+    vim.cmd("hi String gui=None")
+    vim.cmd("hi! link Function GruvboxGreen")
+    vim.cmd("hi Normal guibg=#151515")
+    vim.cmd("hi CursorLine guibg=#1d2021")
 end
