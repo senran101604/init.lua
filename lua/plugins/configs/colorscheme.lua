fa02ee458ac199f1f31cde410e609856ade06524
SenRan <senran101604@gmail.com>
2023-12-22 16:10:18 +0800

    Use terminal's background color

--- lua/plugins/configs/colorscheme.lua
+++ lua/plugins/configs/colorscheme.lua
@@ -4,11 +4,11 @@ local current_colorscheme = vim.g.colors_name
 
 -- Melange
 if current_colorscheme == "melange" then
-    vim.cmd("hi Normal guibg=#151515")
+    -- vim.cmd("hi Normal guibg=#151515")
     vim.cmd("hi NormalFloat guibg=#292522")
     -- vim.cmd("hi ColorColumn guibg=#292522")
     vim.cmd("hi String gui=None")
-    -- vim.cmd("hi Normal guibg=None")
+    vim.cmd("hi Normal guibg=None")
     vim.cmd("hi ColorColumn guibg=None")
 -- TeraFox from nightfox.nvim
 elseif current_colorscheme == "terafox" then
