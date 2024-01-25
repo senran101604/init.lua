deef71c61d35688e5e9e8e625397526bbd3705e4
SenRan <senran101604@gmail.com>
2024-01-25 15:34:46 +0800

    Back to original gruvb colors

--- lua/plugins/configs/colorscheme.lua
+++ lua/plugins/configs/colorscheme.lua
@@ -1,5 +1,6 @@
 -- Highlighting Configurations
-vim.cmd.colorscheme 'melange'
+-- vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme 'gruvbox'
 
 local current_colorscheme = vim.g.colors_name
 
@@ -9,22 +10,22 @@ if current_colorscheme == "melange" then
 elseif current_colorscheme == "gruvbox" then
     -- vim.cmd("hi Normal      guifg=#FFD8B1")
     -- vim.cmd("hi GruvboxFg1  guifg=#FFD8B1")
-    vim.cmd("hi Normal          guifg=#F5F2EB")
-    vim.cmd("hi GruvboxFg1      guifg=#F5F2EB")
-    vim.cmd("hi PmenuSel        guibg=#FFA07A")
-    vim.cmd("hi Underlined      guifg=#B3CCE3   gui=bold,underline")
-    vim.cmd("hi String          guifg=#DADFAE   gui=None")
+    -- vim.cmd("hi Normal          guifg=#F5F2EB")
+    -- vim.cmd("hi GruvboxFg1      guifg=#F5F2EB")
+    -- vim.cmd("hi PmenuSel        guibg=#FFA07A")
+    -- vim.cmd("hi String          guifg=#DADFAE   gui=None")
+    vim.cmd("hi String          gui=None")
     vim.cmd("hi textSnipTEX     guifg=#FFD8B1")
     -- Gruvbox Colors Customization
-    vim.cmd("hi GruvboxRed          guifg=#FF6666")
-    vim.cmd("hi GruvboxGreen        guifg=#A9C388")
-    vim.cmd("hi GruvboxAqua         guifg=#86C990")
-    vim.cmd("hi GruvboxYellow       guifg=#E5C07B")
-    vim.cmd("hi GruvboxOrange       guifg=#FFA07A")
-    vim.cmd("hi GruvboxBlue         guifg=#B3CCE3")
+    -- vim.cmd("hi GruvboxRed          guifg=#FF6666")
+    -- vim.cmd("hi GruvboxGreen        guifg=#A9C388")
+    -- vim.cmd("hi GruvboxAqua         guifg=#86C990")
+    -- vim.cmd("hi GruvboxYellow       guifg=#E5C07B")
+    -- vim.cmd("hi GruvboxOrange       guifg=#FFA07A")
+    -- vim.cmd("hi GruvboxBlue         guifg=#B3CCE3")
     vim.cmd("hi! link Function      GruvboxGreen")
     vim.cmd("hi! link FloatBorder   GruvboxGray")
-    vim.cmd("hi! link Operator      GruvboxOrange")
+    -- vim.cmd("hi! link Operator      GruvboxOrange")
 elseif current_colorscheme == "catppuccin-mocha" then
     vim.cmd("hi Conditional         gui=None")
 end
@@ -49,7 +50,6 @@ vim.cmd("hi VimwikiHeaderChar       guifg=#fab387")
 vim.cmd("hi Todo                guibg=#292522 guifg=#d4b55e")
 vim.cmd("hi Normal              guibg=None")
 vim.cmd("hi Underlined          guifg=#cba6f7 gui=underline")
-vim.cmd("hi ColorColumn         guibg=#1d2021")
 vim.cmd("hi Comment             gui=None")
 vim.cmd("hi! link NormalNC      Normal")
 vim.cmd("hi! link NormalFloat   Normal")
